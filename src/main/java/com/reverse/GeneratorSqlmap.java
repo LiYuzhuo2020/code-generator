6679cf7ea9d871e6ee0015d549b937c7f9cbb5c0
drainli <905817024@qq.com>
2021-05-03 17:00:31 +0800

    mybatis代码代码生成器

---
+++ src/main/java/com/reverse/GeneratorSqlmap.java
@@ -0,0 +1,41 @@
+package com.reverse;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: drainli
+ * @Description: mybatis逆向工程生成简单的CRUD功能
+ * @Date: 2021/5/2 21:07
+ **/
+public class GeneratorSqlmap {
+
+    public void generator() throws Exception{
+
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("E:\\workspace_intelij\\mybatis-reverse\\src\\main\\resources\\generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+
+    }
+    public static void main(String[] args) throws Exception {
+        try {
+            GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+            generatorSqlmap.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
